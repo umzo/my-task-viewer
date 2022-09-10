7b009aaf4db2b13f33222cb5de972512ad8068c2
umzo <umeda2444@gmail.com>
2022-09-10 16:31:00 +0900

    alias設定

---
+++ frameworks/web/src/providers/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { BrowserRouter } from "react-router-dom";
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  return (
+    <BrowserRouter>
+      {children}
+    </BrowserRouter>
+  );
+};
---
+++ frameworks/web/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { useRoutes} from "react-router-dom";
+import Home from"@/pages/home" 
+import Page1 from"../pages/page1" 
+import Page2 from"../pages/page2" 
+
+export const AppRoutes = () => {
+  const commonRoutes = [
+    { path: '/', element: <Home /> },
+    { path: '/page1/', element: <Page1 /> },
+    { path: '/page2/', element: <Page2 /> },
+
+  ];
+  const element = useRoutes([...commonRoutes]);
+  return <>{element}</>
+}
